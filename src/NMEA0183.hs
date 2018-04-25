292a4c2b7471aad48626e2115e193d274a722007
Sergii Perynskyi <sergebass@gmail.com>
2018-04-24 23:36:35 -0400

    Add NMEA-0183 sentence format documentation and prepare stubs for decoding.

--- src/NMEA0183.hs
+++ src/NMEA0183.hs
@@ -25,8 +25,12 @@ transformCSV :: CSV -> IO ()
 transformCSV csv = do
   mapM_ (putStrLn . show) (csvToNMEARecords csv)
 
-data NMEARecord = GPGGA [Field]
-                | GPRMC [Field]
+-- FIXME use proper records here
+data NMEARecord = GGA [Field]  -- Global Positioning System Fix Data
+                | GSA [Field]  -- GPS DOP and active satellites
+                | GSV [Field]  -- Satellites in view
+                | RMC [Field]  -- Recommended Minimum Navigation Information
+                | VTG [Field]  -- Track Made Good and Ground Speed
                 deriving Show
 
 csvToNMEARecords :: CSV -> [Maybe NMEARecord]
@@ -35,9 +39,110 @@ csvToNMEARecords csv = map recordToNMEARecord csv
 recordToNMEARecord :: Record -> Maybe NMEARecord
 recordToNMEARecord (recordType : fields) =
     case recordType of
-        "$GPGGA" -> Just (GPGGA fields)
-        "$GPRMC" -> Just (GPRMC fields)
+        "$GPGGA" -> decodeGGA fields
+        "$GPGSA" -> decodeGSA fields
+        "$GPGSV" -> decodeGSV fields
+        "$GPRMC" -> decodeRMC fields
+        "$GPVTG" -> decodeVTG fields
         otherwise -> Nothing
+
+-- GGA: Global Positioning System Fix Data. Time, Position and fix related data
+-- for a GPS receiver
+--
+--        1         2       3 4        5 6 7  8   9  10 11 12 13  14   15
+--        |         |       | |        | | |  |   |   | |   | |   |    |
+-- $--GGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+--  1) Time (UTC)
+--  2) Latitude
+--  3) N or S (North or South)
+--  4) Longitude
+--  5) E or W (East or West)
+--  6) GPS Quality Indicator,
+--     0 - fix not available,
+--     1 - GPS fix,
+--     2 - Differential GPS fix
+--  7) Number of satellites in view, 00 - 12
+--  8) Horizontal Dilution of precision
+--  9) Antenna Altitude above/below mean-sea-level (geoid)
+-- 10) Units of antenna altitude, meters
+-- 11) Geoidal separation, the difference between the WGS-84 earth ellipsoid and mean-sea-level (geoid), "-" means mean-sea-level below ellipsoid
+-- 12) Units of geoidal separation, meters
+-- 13) Age of differential GPS data, time in seconds since last SC104 type 1 or 9 update, null field when DGPS is not used
+-- 14) Differential reference station ID, 0000-1023
+-- 15) Checksum
+decodeGGA :: [Field] -> Maybe NMEARecord
+decodeGGA fields = Just (GGA fields)  -- TODO
+
+-- GSA: GPS DOP and active satellites
+--
+--        1 2 3                         14 15  16  17 18
+--        | | |                         |  |   |   |  |
+-- $--GSA,a,a,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x.x,x.x,x.x*hh
+--  1) Selection mode
+--  2) Mode
+--  3) ID of 1st satellite used for fix
+--  4) ID of 2nd satellite used for fix
+--  ...
+-- 14) ID of 12th satellite used for fix
+-- 15) PDOP in meters
+-- 16) HDOP in meters
+-- 17) VDOP in meters
+-- 18) Checksum
+decodeGSA :: [Field] -> Maybe NMEARecord
+decodeGSA fields = Just (GSA fields)  -- TODO
+
+-- GSV: Satellites in view
+--
+--        1 2 3 4 5 6 7     n
+--        | | | | | | |     |
+-- $--GSV,x,x,x,x,x,x,x,...*hh
+-- 1) total number of messages
+-- 2) message number
+-- 3) satellites in view
+-- 4) satellite number
+-- 5) elevation in degrees
+-- 6) azimuth in degrees to true
+-- 7) SNR in dB
+-- more satellite infos like 4)-7)
+-- n) Checksum
+decodeGSV :: [Field] -> Maybe NMEARecord
+decodeGSV fields = Just (GSV fields)  -- TODO
+
+-- RMC: Recommended Minimum Navigation Information
+--
+--        1         2 3       4 5        6 7   8   9    10  11 12
+--        |         | |       | |        | |   |   |    |   |  |
+-- $--RMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,xxxx,x.x,a*hh
+--  1) Time (UTC)
+--  2) Status, V = Navigation receiver warning
+--  3) Latitude
+--  4) N or S
+--  5) Longitude
+--  6) E or W
+--  7) Speed over ground, knots
+--  8) Track made good, degrees true
+--  9) Date, ddmmyy
+-- 10) Magnetic Variation, degrees
+-- 11) E or W
+-- 12) Checksum
+decodeRMC :: [Field] -> Maybe NMEARecord
+decodeRMC fields = Just (RMC fields)  -- TODO
+
+-- VTG: Track Made Good and Ground Speed
+--        1   2 3   4 5 6 7   8 9
+--        |   | |   | | | |   | |
+-- $--VTG,x.x,T,x.x,M,x.x,N,x.x,K*hh
+-- 1) Track Degrees
+-- 2) T = True
+-- 3) Track Degrees
+-- 4) M = Magnetic
+-- 5) Speed Knots
+-- 6) N = Knots
+-- 7) Speed Kilometers Per Hour
+-- 8) K = Kilometres Per Hour
+-- 9) Checksum
+decodeVTG :: [Field] -> Maybe NMEARecord
+decodeVTG fields = Just (VTG fields)  -- TODO
 
 type Latitude = Float
 type Longitude = Float
