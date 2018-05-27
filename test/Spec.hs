9da17eed56a95ba866b44c2196727714a3914c3b
Sergii Perynskyi <sergebass@gmail.com>
2018-05-26 22:47:08 -0400

    Add initial configuration for Hspec and QuickCheck.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "NMEA0183" $ do
+        it "FIXME does something 1" $ do
+            2 + 2 `shouldBe` 4
+        it "FIXME does something 2" $ do
+            property $ \x xs -> head (x:xs) == (x :: Int)
